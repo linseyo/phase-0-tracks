2ad6fe233fbbbf14ab9bfdff04f32469ec370fbd
linseyo <reedlinsey@gmail.com>
2017-02-02 22:00:18 -0800

    debug

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -18,22 +18,22 @@ puts "Welcome Interior Designers, Please enter Client info"
 application = Hash.new
 
 puts "What is your clients name?"
-application [:name] = gets.chomp
+application [:"name"] = gets.chomp
 
 puts "What is their age?"
-application [:age] = gets.chomp
+application [:"age"] = gets.chomp
 
 puts "How many kids do they have?"
-application [:number_of_kids] = gets.chomp
+application [:"number_of_kids"] = gets.chomp
 
 puts "Prefered decor theme"
-application [:decor_theme] = gets.chomp
+application [:"decor_theme"] = gets.chomp
 
-puts "What type of pets do you have?"
-application [:pets] = gets.chomp
+# puts "What type of pets do you have?"
+# application [:"pets"] = gets.chomp
 
-puts "Do you prefer carpet or hardwood floors"
-application [:floors] = gets.chomp
+# puts "Do you prefer carpet or hardwood floors"
+# application [:"floors"] = gets.chomp
 
 #prints hash
 puts application
@@ -53,7 +53,6 @@ end
 
 
 puts application[edit] = changes  # <--- use to view changes to hash
-
 
 puts application
 puts "Thanks for fillin' this out!"
