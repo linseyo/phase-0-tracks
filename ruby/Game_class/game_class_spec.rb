9806f05bbcf019eb0cf656cb63d731d604c30d26
linseyo <reedlinsey@gmail.com>
2017-02-11 08:58:45 -0800

    run test for failure method

--- ruby/Game_class/game_class_spec.rb
+++ ruby/Game_class/game_class_spec.rb
@@ -3,6 +3,14 @@
 require_relative 'game'
 
 describe Game do
+
+
+
+	it "prints out taunting message" do
+		game = Game.new
+		expect(game.failure).to eq 'Sorry you lose'
+	end
+
 	it "prints out congrats message" do
 		game = Game.new
 		expect(game.congrats).to eq 'Congrats on Guessing the Word'
