528dcd681270e15722b03e1bc98bcc737ca2ca05
linseyo <reedlinsey@gmail.com>
2017-02-10 16:55:31 -0800

    run test on congrats code

--- ruby/Game_class/game_class_spec.rb
+++ ruby/Game_class/game_class_spec.rb
@@ -1,8 +1,8 @@
-require_relative 'game_class'
+require_relative 'game'
 
-describe game_class do
+describe Game do
 	it "prints out congrats message" do
-		game = Game_class.new
+		game = Game.new
 		expect(game.congrats).to eq "Congrats on Guessing the Word!"
 	end
 	
