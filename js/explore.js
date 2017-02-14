0df207b4dc5cdb1895b7ad83b2407ba73e0333bf
linseyo <reedlinsey@gmail.com>
2017-02-14 06:36:18 -0800

    Initial Pseudocode

---
+++ js/explore.js
@@ -0,0 +1,10 @@
+// Pseudocode a function that take a string as a parameter and reverse the string
+//i.e: reverse("hello") should retrun "olleh"
+//Function:
+	//Enter String
+	//Reverse String
+//Drive Code: Calls the function 
+	//Enter String
+	//Reverse String
+	//Store the result of the function in a variable
+//Prints the variable if some condition is true (it can be silly like 1 == 1)
