1f4ef56588215899c09f8b7788fae9a5183d710e
linseyo <reedlinsey@gmail.com>
2017-02-14 18:04:57 -0800

    add pseudocode and possible function code:

--- js/explore.js
+++ js/explore.js
@@ -3,8 +3,19 @@
 //Function:
 	//Enter String
 	//Reverse String
-//Drive Code: Calls the function 
-	//Enter String
-	//Reverse String
+
+var string = ''
+var truth = true
+
+function reverseStr() {
+	console.log()
+}
+//Drive Code: 
+	//Calls the function 
 	//Store the result of the function in a variable
-//Prints the variable if some condition is true (it can be silly like 1 == 1)
+	//Prints the variable if some condition is true (it can be silly like 1 == 1)
+
+if truth {
+	reverseStr();
+}
+console.log(string);
