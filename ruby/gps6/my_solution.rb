e0ba524628d6c065cdd6f0d86a31ffbe8eac8df0
linseyo <reedlinsey@gmail.com>
2017-02-15 20:15:26 -0800

    update code to match partner during gps

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -4,59 +4,90 @@
 # We spent [#] hours on this challenge.
 
 # EXPLANATION OF require_relative
-#
+# require_relative allows you to access the file you want by typing the access path from your current location on your computer.
+# with require, you would need to type out the entire path to get to the file you want to work with. 
 #
 require_relative 'state_data'
 
 class VirusPredictor
+#Initialize assigns attributes (characteristics) to program and assigns the values that are input in the Driver Code with the parameters.
 
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
+#Calls 2 other methods. Assigns attributes to the called methods. 
 
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths#(@population_density, @population, @state) <----
+    speed_of_spread #(@population_density, @state) <----- No Parameters needed. Attributes established
   end
 
   private
-
-  def predicted_deaths(population_density, population, state)
+#Conditional based on population density attribute. 
+#More population density = more deaths
+  def predicted_deaths#(population_density, population, state) <---- Did not need parameters. Attributes are already in initialize.
     # predicted deaths is solely based on population density
-    if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
-    elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
-    elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
-    elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
-    else
-      number_of_deaths = (@population * 0.05).floor
+  #MAKE IT MORE DRY
+  #Create until loop where population density and float coefficient decrease by 50 and 0.1 each loop, respectively.  
+    pop_den = 200
+    x = 0.4
+    until pop_den == 0 
+      if @population_density >= pop_den
+        number_of_deaths = (@population * x).floor
+      else @population_density < 50
+        number_of_deaths = (@population * 0.05).floor
+      end 
+      pop_den -= 50
+      x -= 0.1
     end
+    # if @population_density >= 200
+    #   number_of_deaths = (@population * 0.4).floor
+    # elsif @population_density >= 150
+    #   number_of_deaths = (@population * 0.3).floor
+    # elsif @population_density >= 100
+    #   number_of_deaths = (@population * 0.2).floor
+    # elsif @population_density >= 50
+    #   number_of_deaths = (@population * 0.1).floor
+    # else
+    #   number_of_deaths = (@population * 0.05).floor
+    # end
 
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
   end
 
-  def speed_of_spread(population_density, state) #in months
+  def speed_of_spread#(population_density, state) 
+# Conditional that increments the speed value based upon population density. 
+
+  #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
-    speed = 0.0
-
-    if @population_density >= 200
-      speed += 0.5
-    elsif @population_density >= 150
-      speed += 1
-    elsif @population_density >= 100
-      speed += 1.5
-    elsif @population_density >= 50
-      speed += 2
-    else
-      speed += 2.5
+    #speed = 0.0
+    pop_den = 200
+    x = 0.5
+    until pop_den == 0
+      if @population_density >= pop_den
+        speed = x
+      else @population_density < 50
+        speed = 2.5
+      end
+      pop_den -= 50
+      x += 0.5
     end
+
+    # if @population_density >= 200
+    #   speed += 0.5
+    # elsif @population_density >= 150
+    #   speed += 1
+    # elsif @population_density >= 100
+    #   speed += 1.5
+    # elsif @population_density >= 50
+    #   speed += 2
+    # else
+    #   speed += 2.5
+    # end
 
     puts " and will spread across the state in #{speed} months.\n\n"
 
@@ -65,6 +96,14 @@ class VirusPredictor
 end
 
 #=======================================================================
+#Run a .each for each state key in the STATE_DATA hash. 
+#For each of the state keys, we want to run the VirusPredictor program.
+  #Needs to be able to accept state key, population density and population as arguments.  
+STATE_DATA.each do |state, number|
+  state = VirusPredictor.new(state, number[:population_density], number[:population])
+  state.virus_effects
+end
+
 
 # DRIVER CODE
  # initialize VirusPredictor for each state
