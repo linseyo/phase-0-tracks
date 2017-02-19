1391006fbd039a46bca8eb3f6bdae1a61aca3b9e
linseyo <reedlinsey@gmail.com>
2017-02-18 19:51:42 -0800

    create algos file, add pseudocode for releases

---
+++ js/algos.js
@@ -0,0 +1,37 @@
+// Release 0: Find the Longest Phrase
+
+	
+	
+
+
+// Input: array of words or phrases
+var word_array = ["house", "chicken", "egg", "airplane"]
+// Function will take in the array of phrases/words
+	// Iterate through the array and determine the length of each word/phrase in the array
+	// determine from those lengths which is the longest
+function wordlengths(arr)
+	for (var i = 0; i < word_array.length; i++) {
+		console.log(word_array[i]);
+	} 
+
+
+// Output: the longest word or phrase in the array
+
+
+
+//Release 1: Find a Key-Value Match
+//Function will take 2 objects
+//Check if objects share at least one key-value pair
+//return true for a match
+//Otherwise return false
+
+
+
+//Release 2: Generate Random Test Data
+//Input: an integer for length
+//Builds an array
+//Returns an array of strings based on the integer length
+
+
+
+
