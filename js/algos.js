d1d723dcf26985bc3d779b01e500bcc116bb9c3a
linseyo <reedlinsey@gmail.com>
2017-02-18 23:31:54 -0800

    add code to test object key-value matching

--- js/algos.js
+++ js/algos.js
@@ -26,30 +26,33 @@
 var set1 = {name: "John", age: 23}
 var set2 = {name: "Jack", age: 34}
 var set3 = {name: "John", age: 55}
-var set4 = {name: "Bill", age: 34}
+var set4 = {name: "Bill", age: 34, gender: "male"}
 			// Test for various keys-value matches
 //Function will take 2 objects
 function matchpair(var1, var2) {
-// Check if objects share at least one key-value pair
-	// if (var1[0] == var2[0] || var1[1] == var2[1]) {
+// Check if objects share at least one key-value pairs
 	if (var1 == var2) {
 		console.log("true");
-	//return true for a match
+		return true;
+	}
+		// This Conditional statement returns TRUE for all...
+	else if (var1[0] === var2[0] || var1[1] === var2[1]) {
+		// This conditional statement will return FALSE for all...
+		console.log("true");
+	// //return true for a match
 		return true;
 	}
 	else {
 		console.log("false")
-	//Otherwise return false
+	// //Otherwise return false
 		return false;
 	}
 }
 
-//matchpair(set1, set3)
-//matchpair(set2, set4)
+
+matchpair(set1, set3)
+matchpair(set2, set4)
 matchpair(set1, set4)
-
-
-
 
 
 ////////////////////////////////////////
