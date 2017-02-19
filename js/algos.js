3ae194334ee5c289b5aa9bcc95e2f81ac95803e7
linseyo <reedlinsey@gmail.com>
2017-02-19 10:41:43 -0800

    adjust longest_word conditional, add pseudocode for driver code

--- js/algos.js
+++ js/algos.js
@@ -1,23 +1,21 @@
 // Release 0: Find the Longest Phrase
 // Input: array of words or phrases
-// var word_array = ["house", "chicken", "egg", "airplane"]
+var word_array = ["house", "chicken", "egg", "airplane"]
 // // Function will take in the array of phrases/words
 // 	// determine from those lengths which is the longest
-// function wordlengths(arr) {
-// 	var longest_word = ""
-// 	var longest_length = 0
+function wordlengths(arr) {
+	var longest_word = ""
 // // Iterate through the array and determine the length of each word/phrase in the array
-// 	for (var i = 0; i < arr.length; i++) {
-// 		if (arr[i].length > length ) {
-// 			longest_length = arr[i].length;
-// 			longest_word = arr[i]
-// 		}
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[i].length > longest_word.length ) {
+			longest_word = arr[i]
+		}
 		
-// 	} 
-// 	console.log(longest_word)
-// }
+	} 
+	console.log(longest_word)
+}
 // // Output: the longest word or phrase in the array
-// wordlengths(word_array);
+
 
 /////////////////////////////////////////
 
@@ -49,18 +47,32 @@ function matchpair(var1, var2) {
 	}
 }
 
+////////////////////////////////////////
+//Release 2: Generate Random Test Data
+//Input: an integer for length
+function wordgenerator(num) {
+
+}
+	
+//Builds an array of words
+//Words should be of varying length
+//Returns an array of strings based on the integer length
+
+
+
+
+
+/////DRIVER CODE:
+wordlengths(word_array)
+
 
 matchpair(set1, set3)
 matchpair(set2, set4)
 matchpair(set1, set4)
 
-
-////////////////////////////////////////
-//Release 2: Generate Random Test Data
-//Input: an integer for length
-//Builds an array
-//Returns an array of strings based on the integer length
-
-
-
+//does the following 10 times: 
+	//generate an array
+	//prints array
+	//feeds array to wordslengths function
+	//prints results
 
