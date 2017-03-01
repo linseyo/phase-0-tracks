5b93211734814a4ff061ecf6c7b967bfea59a0d6
linseyo <reedlinsey@gmail.com>
2017-02-28 19:55:23 -0800

    use getElementbyId

---
+++ web_dev/client_side_js/panda.js
@@ -0,0 +1,10 @@
+console.log("The script is running!!!")
+
+function changeheader() {
+	var header = document.getElementById("beginning");
+	header.style.border = "10px solid purple";
+}
+
+changeheader();
+
+
